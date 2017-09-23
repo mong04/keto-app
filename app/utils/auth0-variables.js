1aa5f9c44bbd6a8f5170c0018a8dddf3985c743c
Andrew Sanchez <bballbizniz@aol.com>
2017-09-23 11:08:52 -0700

    change callbackURL for local

--- app/utils/auth0-variables.js
+++ app/utils/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: 'keto-app.auth0.com',
   clientId: 'vj2Tz98uE3llbuy3uykxyxy1r3O8yzAD',
-  callbackUrl: 'https://kdiet.herokuapp.com/callback'
+  callbackUrl: 'http://localhost:3000/callback'
 }
