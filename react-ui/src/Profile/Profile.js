7650a5935d66991af0acf5d3b1dae0c10f6a1ba2
Andrew Sanchez <bballbizniz@aol.com>
2017-09-30 10:30:54 -0700

    updated profile

--- react-ui/src/Profile/Profile.js
+++ react-ui/src/Profile/Profile.js
@@ -1,14 +1,59 @@
 import React, { Component } from 'react';
-import { Panel, ControlLabel, Glyphicon } from 'react-bootstrap';
+import { Panel, ControlLabel, Glyphicon, Grid, Row, Col, Button } from 'react-bootstrap';
 import './Profile.css';
 import axios from 'axios';
-
+import moment from 'moment';
+import { Bar, Pie } from 'react-chartjs-2';
 class Profile extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      profile: {}
+      profile: {},
+      chartData: {
+        labels: [ 
+          'BMR', 'Energy Expenditure', 'caloric intake'
+        ],
+        datasets: [
+          { 
+            label: 'Energy',
+            data:[
+              1852,
+              2222,
+              1666,
+            ],
+            backgroundColor: [
+              'rgba(255, 99, 132, .6)',
+              'rgba(255, 130, 132, .6)',
+              'rgba(255,  200, .6)',
+            ]
+          }
+        ]
+      },
+      chartData2: {
+        labels: [ 
+          'protein', 'fat', 'carbs'
+        ],
+        datasets: [
+          { 
+            label: 'Daily Intakes',
+            data:[
+              126,
+              118,
+              25
+            ],
+            backgroundColor: [
+              'rgba(50, 156, 132, .6)',
+              'rgba(50, 205, 132, .6)',
+              'rgba(50, 255, 132, .6)'
+            ]
+          }
+        ]
+      },
+      goals: {}
     }
+  }
+  goTo(route) {
+    this.props.history.replace(`/${route}`)
   }
   componentWillMount() {
     const { userProfile, getProfile } = this.props.auth;
@@ -18,6 +63,7 @@ class Profile extends Component {
         axios.post('/api', {
           userInfo: this.state.profile
         }).then(function(res) {
+          this.setState({ goals: res });
           console.log(res);
         }).catch(function(err) {
           console.log(err);
@@ -32,19 +78,53 @@ class Profile extends Component {
     return (
       <div className="container">
         <div className="profile-area">
-          <h1>{profile.name}</h1>
-          <Panel header="Profile">
-            <img src={profile.picture} alt="profile" />
-            <div>
-              <ControlLabel><Glyphicon glyph="user" /> Nickname</ControlLabel>
-              <h3>{profile.nickname}</h3>
-            </div>
-            <pre>{JSON.stringify(profile, null, 2)}</pre>
+        <Grid>
+          <Panel header="Profile" bsStyle="success">
+          <Row>
+            <Col xs={12} md={6}>
+              <Panel header="Your Info">
+                <p><ControlLabel><Glyphicon glyph="user" /> Currently Logged in as: {profile.name}</ControlLabel></p>
+                <p><ControlLabel><Glyphicon glyph="apple" /> Member Since: {moment(profile.created_at).format('MMMM D, YYYY')}</ControlLabel></p>
+                <p><ControlLabel><Glyphicon glyph="sunglasses" /> Nickname: {profile.nickname}</ControlLabel></p>
+              </Panel>
+            </Col>
+            <Col md={1} />
+            <Col xs={12} md={4} >
+              <Button bsStyle="success" onClick={this.goTo.bind(this, 'calculator')}>
+                Calculate Your Goals Here
+              </Button>
+            </Col>
+          </Row>
+          <Row>
+          <Row>
+            <h5>Energy Expenditure</h5>
+          </Row>
+          <Bar
+              data={this.state.chartData}
+              height={50}
+              width={50}
+              wid
+              options={{
+                maintainAspectRatio: false
+              }}
+            />
+            </Row>
+            <h5>Daily Intakes</h5>
+            <Row>
+            <Pie
+              data={this.state.chartData2}
+              height={200}
+              width={200}
+              options={{
+                maintainAspectRatio: false
+              }}
+            />
+          </Row>
           </Panel>
+          </Grid>
         </div>
       </div>
     );
   }
 }
-
 export default Profile;
